ec2402854866f64c5a51d4ca4a561f049e39094b
Mubashir Hassan <official.mhm13@gmail.com>
2022-12-15 00:23:10 +0500

    Learned: - to import a contract from another contract - to deploy contract from inside of another contract - Factory Contract Concept

---
+++ StorageFactory.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        // To interact with any contract, we need 2 things
+        // Contract Address
+        // ABI - available in this file because of import
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+}
